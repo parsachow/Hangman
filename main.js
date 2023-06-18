c9dfa7c16d4dfc5053edd8154a1fb0c0fceb3c95
parsachow <parsakishwa06@gmail.com>
2023-06-18 13:23:56 -0400

    more hangman updates

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
-  //list of movies: THE DARK KNIGHT, PULP FICTION, THE LORD OF THE RINGS RETURN OF THE KING, FORREST GUMP, FIGHT CLUB, INCEPTION, THE MATRIX, JOHN WICK, SPIRITED AWAY, PARASITE, INDIANA JONES RAIDERS OF THE LOST ARK, THE USUAL SUSPECTS, THE ITALIAN JOB, CATCH ME IF YOU CAN, MONSTERS INC, BOURNE IDENTITY, THE FUNDAMENTALS OF CARING
-
   /*----- constants -----*/
 
-  const IMGAGES = {img1: "/pics/hangman1.jpeg", img2: "pics/hangman2.jpeg", img3: "pics/hangman3.jpeg", img4: "pics/hangman4.jpeg", img5: "pics/hangman5.jpeg", img6: "pics/hangman6.jpeg"}
+  const PICTURES = [
+  {img1: "/pics/hangman1.jpeg"}, 
+  {img2: "pics/hangman2.jpeg"}, 
+  {img3: "pics/hangman3.jpeg"}, 
+  {img4: "pics/hangman4.jpeg"}, 
+  {img5: "pics/hangman5.jpeg"}, 
+  {img6: "pics/hangman6.jpeg"},
+  {imgDefault: "pics/hangmanRopeMain.jpeg"}]
 
-  const movieList = ["MONSTERS INC", "BOURNE IDENTITY", "THE FUNDAMENTALS OF CARING"];
+  const movieList = ["MONSTERS INC", "BOURNE IDENTITY", "THE FUNDAMENTALS OF CARING", "THE DARK KNIGHT", "PULP FICTION", "THE LORD OF THE RINGS RETURN OF THE KING", "FORREST GUMP", "FIGHT CLUB", "INCEPTION", "THE MATRIX", "JOHN WICK", "SPIRITED AWAY", "PARASITE", "THE USUAL SUSPECTS", "THE ITALIAN JOB", "CATCH ME IF YOU CAN"];
 
   /*----- state variables -----*/
 
-  let winCount; //number/counting of wins
-  let lossCount;  //number/counting of loss
-  let hiddenWord; //selected word
+  let win; //winning msg
+  let loss;  //losing msg
+  //let selectedWord; //hidden word picked
   let guessedletter; //letters picked from the array/alphabets
   let incorrectGuess; //max 5 allowed, 6th is counted as loss
   let blankSpace; //length of spaces in 'word'
@@ -18,28 +23,53 @@
 
   /*----- cached elements  -----*/
 
+  const image = document.getElementById("hangMain");
   const userInput = document.getElementById("blank-spaces");
   const lettersToPick = document.getElementById("alphabets");
   const newGameBtn = document.getElementById("new-game")
-  const playAgainBtn = document.getElementById("play-again")
   
-
+  
   /*----- event listeners -----*/
 
-  //document.querySelector('main').addEventListener('click', handleClicks);
+  //document.querySelector('#board').addEventListener('click', handleClicks);
 
 
   /*----- functions -----*/
 
+  function defImg(){
+    image.src = PICTURES[6].imgDefault;
+    
+  }
+  defImg();
+
+  const alpha = Array.from(Array(26)).map((e, i) => i + 65);
+  const alphabet = alpha.map((x) => String.fromCharCode(x));
+  console.log(alphabet)
+
+function createButton() {
+  
+  for (let i=0; i<alphabet.length; i++){
+  document.getElementById("alphabets").innerHTML += "<button>" + alphabet[i] + "</button>"
+}}
+
+createButton();
+
+  function randomWord(movieList) {
+    return movieList[Math.floor(Math.random()*movieList.length)];
+  }
+
+
+  function blankSpaces() {
+    selectWord = randomWord(movieList);
+    selectWordLength = selectWord.length;
+  }
+  blankSpaces();
+  console.log(selectWord, selectWordLength);
+  
   /*init();
 
   function init() {
     
-   winCount = 0;
-   lossCount = 0;
-   //blankSpace = movieList[i(str.length)];
-
-
     render();
   }
 
@@ -78,20 +108,6 @@
 
  
 
-    const alpha = Array.from(Array(26)).map((e, i) => i + 65);
-    const alphabet = alpha.map((x) => String.fromCharCode(x));
-    console.log(alphabet)
-
-  function createButton() {
-    
-    for (let i=0; i<alphabet.length; i++) {
-    /*lettersToPick = document.getElementById("alphabets");
-    let btn = document.createElement("button");
-    btn.textContent=lettersToPick[i];
-    lettersToPick.append(btn);*/
  
-    document.getElementById("alphabets").innerHTML += "<button>" + alphabet[i] + "</button>"
 
-  }}
-
-  createButton();
+  
