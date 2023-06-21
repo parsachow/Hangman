cdcc8d492e864c04b9abba6bb1795ca40ddf23af
parsachow <parsakishwa06@gmail.com>
2023-06-21 12:39:31 -0400

    html & js code update

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-//const movieList = ["MONSTERS INC", "BOURNE IDENTITY", "THE FUNDAMENTALS OF CARING", "THE DARK KNIGHT", "PULP FICTION", "THE LORD OF THE RINGS RETURN OF THE KING", "FORREST GUMP", "FIGHT CLUB", "INCEPTION", "THE MATRIX", "JOHN WICK", "SPIRITED AWAY", "PARASITE", "THE USUAL SUSPECTS", "THE ITALIAN JOB", "CATCH ME IF YOU CAN"];
   
   /*----- constants -----*/
 
@@ -11,14 +10,13 @@
   {img6: "pics/hangman6.jpeg"},
   {imgDefault: "pics/hangmanRopeMain.jpeg"}]
 
-  const movieList = ["ALIEN", "INCEPTION", "ARMAGEDDON", "PARASITE", "PULP FICTION"];
+  const movieList = ["ALIEN", "INCEPTION", "ARMAGEDDON", "PARASITE", "JAWS", "GLADIATOR", "TANGLED", "ENCHANTED", "ARGO"];
 
   /*----- state variables -----*/
 
   let win; //winning msg
   let loss;  //losing msg
   let selectWord; //hidden word picked
-  let guessedLetterIdx; //letters picked from the array/alphabets
   let incorrectGuess; //max 5 allowed, 6th is counted as loss
   let wordLength; //length of spaces in 'word'
   let hiddenWord = [];
@@ -30,30 +28,35 @@
   const userInput = document.getElementById("blank-spaces");
   const lettersToPick = document.getElementById("alphabets");
   const newGameBtn = document.getElementById("new-game")
+  const message = document.getElementById("board");
   
   
   /*----- event listeners -----*/
 
   lettersToPick.addEventListener('click', handleClicks);
-
+  
 
   /*----- functions -----*/
 
+function checkWinner(){
+  //if all blank spaces are filled- winning msg
+  //if not - image changes - losing msg after 5th try. picture change
+}
 
 
+
+ //if selectword includes the letter from evt.target.innerText
+ //then the letter(s) should show up on the text section in the correct place
+ //select blank-spaces then update the textContent property
+ //otherwise it should show the wrong answer's picture
 
 function handleClicks(evt){
   evt.preventDefault()
   if (evt.target.tagName !== "BUTTON") return
-  console.log(evt.target.innerText);
-  console.log(selectWord);
+ 
 
-  //if selectword includes the letter from evt.target.innerText
- //then the letter(s) should show up on the text section in the correct place
- //select blank-spaces then update the textContent property
- //otherwise it should show the wrong answer's picture
  selectWord.split("").forEach((letter, idx) => {
-  console.log(letter)
+  //console.log(letter)
 if(evt.target.innerText === letter){
   hiddenWord[idx] = evt.target.innerText
 }
@@ -63,38 +66,15 @@ if (!selectWord.includes(evt.target.innerText)){
  }
 render();
 }
-
-
-  // if(selectWord.includes(evt.target.innerText)) {
-    
-  //   guessedLetterIdx = selectWord.indexOf(evt.target.innerText)
-
-  //   pickedLetterIdx = [];
-
-  //   for(let i=0; i <selectWord.length; i++){
-  //     if(selectWord[i] === evt.target.innerText) 
-  //     pickedLetterIdx.push(i);
-  //     console.log(pickedLetterIdx);
-      
-  //   }
-  // }else {
-  //   image.src = PICTURES[0].img1;
-  //   }}
   
- 
-  
-
-
 
 function defImg(){
     image.src = PICTURES[6].imgDefault;
   }
-defImg();
+
 
   const alpha = Array.from(Array(26)).map((e, i) => i + 65);
   const alphabet = alpha.map((x) => String.fromCharCode(x));
-  console.log(alphabet)
-
 
 
 function createButton() {
@@ -110,9 +90,6 @@ function randomWord(movieList) {
   }
 
 
-
-
-
 function render() {
   document.getElementById("blank-spaces").innerHTML = "";
     for(let i=0; i<wordLength; i++){
@@ -123,6 +100,7 @@ function render() {
   
 
   function init() {
+    defImg();
     selectWord = randomWord(movieList);
     wordLength = selectWord.length;
 
@@ -137,39 +115,4 @@ function render() {
 
 console.log(selectWord, wordLength);
 
-
-
-
-    
-
-//     for (let i=0; i< PICTURES.length[5]; i++){
-//       PICTURES[i]
-//       //updatedPic = PICTURES.map(img => `<img src="$(img)">`)
-//       //image.innerHTML = updatedPic
-
-//       //OR, ???
-//       //img.innerHTML.append("hangMain");
-//     }
-// }
-//  }
-  
-
-  
-  /*
-
-
-  
-
-  
-
-
-  function renderResults() {
-    //calculate win & loss counts
-  }
-
-  
-
-  /*function refreshPage(){
-    window.location.reload();
-    console.log("button clicked")*/
 
